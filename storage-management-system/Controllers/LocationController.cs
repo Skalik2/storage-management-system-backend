c3f8fdd172343194a97c19fdc10132105b68466a
Skalik <106670239+Skalik2@users.noreply.github.com>
2024-12-03 17:40:24 +0100

    Location controller GET PUT DELETE POST methods

---
+++ storage-management-system/Controllers/LocationController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using storage_management_system.Data;
+using storage_management_system.Model.DataTransferObject;
+using storage_management_system.Model.Entities;
+
+namespace storage_management_system.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LocationController : ControllerBase
+    {
+        private readonly PgContext _context;
+
+        public LocationController(PgContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("AddLocation")]
+        public async Task<IActionResult> AddLocation([FromBody] LocationDto locationDto)
+        {
+            if (locationDto == null || string.IsNullOrWhiteSpace(locationDto.Address))
+            {
+                return BadRequest("Address is required.");
+            }
+
+            var newLocation = new Location
+            {
+                Address = locationDto.Address
+            };
+
+            _context.Locations.Add(newLocation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetLocationById), new { id = newLocation.Id }, newLocation);
+        }
+
+        [HttpGet("GetLocationById")]
+        public async Task<IActionResult> GetLocationById(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+
+        [HttpPut("UpdateLocation")]
+        public async Task<IActionResult> UpdateLocation(int id, [FromBody] LocationDto locationDto)
+        {
+            if (locationDto == null || string.IsNullOrWhiteSpace(locationDto.Address))
+            {
+                return BadRequest("Address is required.");
+            }
+
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound($"Location with ID {id} not found.");
+            }
+
+            location.Address = locationDto.Address;
+
+            _context.Locations.Update(location);
+            await _context.SaveChangesAsync();
+
+            return Ok(location);
+        }
+
+        [HttpDelete("DeleteLocation")]
+        public async Task<IActionResult> DeleteLocation(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound($"Location with ID {id} not found.");
+            }
+
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ storage-management-system/Model/DataTransferObject/LocationDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace storage_management_system.Model.DataTransferObject
+{
+    public class LocationDto
+    {
+        public required string Address { get; set; }
+    }
+}
